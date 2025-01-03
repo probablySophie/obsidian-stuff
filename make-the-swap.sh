ddde8c170efbc7a381b7caa1735e783d132f56cd
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-01-03 11:41:26 +0800

    my script to swap out the files

---
+++ make-the-swap.sh
@@ -0,0 +1,6 @@
+. lucide-generator.sh
+
+rm ~/Documents/Obsidian/Test\ Vault/_Meta/mega\ custom\ callout\ list.md ~/Documents/Obsidian/Test\ Vault/.obsidian/snippets/mega\ custom\ callouts.css
+
+cp mega\ custom\ callouts.css ~/Documents/Obsidian/Test\ Vault/.obsidian/snippets/
+cp mega\ custom\ callout\ list.md ~/Documents/Obsidian/Test\ Vault/_Meta/
